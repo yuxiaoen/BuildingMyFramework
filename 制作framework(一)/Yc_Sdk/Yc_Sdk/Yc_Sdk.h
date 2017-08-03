9ab80b8a831bd980c75151459e2158733b73266c
jett_yu <285179335@qq.com>
2017-08-03 19:33:48 +0800

    制作属于自己的SDK第一步

---
+++ 制作framework(一)/Yc_Sdk/Yc_Sdk/Yc_Sdk.h
@@ -0,0 +1,15 @@
+//
+//  Yc_Sdk.h
+//  Yc_Sdk
+//
+//  Created by yu on 2017/8/3.
+//  Copyright © 2017年 jett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Yc_Sdk : NSObject
+
+-(void)printLog;
+
+@end
---
+++ 制作framework(一)/Yc_Sdk/Yc_Sdk/Yc_Sdk.m
@@ -0,0 +1,17 @@
+//
+//  Yc_Sdk.m
+//  Yc_Sdk
+//
+//  Created by yu on 2017/8/3.
+//  Copyright © 2017年 jett. All rights reserved.
+//
+
+#import "Yc_Sdk.h"
+
+@implementation Yc_Sdk
+
+-(void)printLog{
+    NSLog(@"Hello,World");
+}
+
+@end
